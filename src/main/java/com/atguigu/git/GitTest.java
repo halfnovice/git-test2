5cc64dea40f71251e9d51cfde80ca50a1677b4cb
halfnovice <97165679+halfnovice@users.noreply.github.com>
2022-05-07 18:45:30 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hallo git4!");
         System.out.println("master test");
         System.out.println("hot-fix test ");
+        System.out.println("test1");
     }
 }
