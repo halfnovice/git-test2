24c31f44ad34099e4c5465c6e59ed8c78735b3df
fzy <fzy@atguigu.com>
2022-05-07 14:55:07 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hallo git!");
         System.out.println("hallo git2");
+        System.out.println("hallo git3!");
     }
 }
